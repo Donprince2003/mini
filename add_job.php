2382a3179e4502a004fdfe78942c475f2eec34c8
Don Prince <donprince2003428@gmail.com>
2024-08-07 01:37:01 +0530

    3.1st review

---
+++ add_job.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include("conn.php");
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
+    $user_id = $_POST['user_id'];
+    $worker_id = $_POST['user_worker_id'];
+    $job_date = $_POST['job_date'];
+    $job_work = $_POST['job_work'];
+
+    $sql = "INSERT INTO job_tab (job_user, job_worker, job_date, job_work) VALUES ('$user_id', '$worker_id', '$job_date', '$job_work')";
+    if (mysqli_query($conn, $sql)) {
+        echo "<script>alert('Job successfully added!');</script>";
+    } else {
+        echo "<script>alert('Error adding job.');</script>";
+    }
+
+    mysqli_close($conn);
+} else {
+    echo "Invalid request.";
+}
+header("Location: home.php");
+?>
